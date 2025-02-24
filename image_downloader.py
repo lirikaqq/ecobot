e9b6bef7af48055292b3d75de913871ae8bc38e5
lirikaqq <tonnariss@gmail.com>
2025-02-24 18:10:46 +0300

    Add files via upload

---
+++ image_downloader.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+
+def download_image(url):
+    try:
+        response = requests.get(url).text
+        soup = BeautifulSoup(response, 'html.parser')
+
+        # Найти элемент <picture>
+        picture_tag = soup.find('picture', class_='info-block-image-wrapper')
+        if picture_tag:
+            # Попробуем найти первый доступный srcset или src
+            image_url = None
+            source_tags = picture_tag.find_all('source')
+            for source in source_tags:
+                if 'srcset' in source.attrs:
+                    image_url = source['srcset'].split()[0]  # Берем первый URL из srcset
+                    break
+
+            if not image_url:
+                img_tag = picture_tag.find('img')
+                if img_tag and 'src' in img_tag.attrs:
+                    image_url = img_tag['src']
+
+            if image_url and not image_url.startswith('http'):
+                image_url = 'https://www.rbc.ru' + image_url
+            return image_url
+
+        # Если <picture> не найден, попробуем найти <img> с классом 'picture-image'
+        img_tag = soup.find('img', class_='picture-image')
+        if img_tag and 'src' in img_tag.attrs:
+            image_url = img_tag['src']
+            if not image_url.startswith('http'):
+                image_url = 'https://www.rbc.ru' + image_url
+            return image_url
+
+    except Exception as e:
+        print(f"Ошибка при загрузке изображения: {e}")
+    return None
